b4a530bc5d988a6a4fc6526e9368586fedb1328a
Brett Heithold <35582670+baheithold@users.noreply.github.com>
2019-10-14 08:28:57 -0500

    Created a google maps activity

--- app/src/main/java/com/cs495/phototk/MainActivity.java
+++ app/src/main/java/com/cs495/phototk/MainActivity.java
@@ -2,13 +2,59 @@ package com.cs495.phototk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constants
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (isServicesOK()) {
+            init();
+        }
+    }
+
+    private Boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+        int isAvailable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            // Google Play Services is working
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(isAvailable)) {
+            Log.e(TAG, "isServicesOK: A user resolvable error occurred");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, isAvailable, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "Map requests cannot be made", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void init() {
+        Button btnMap = findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
