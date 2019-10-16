08eac59f734faa6d6e34ab541baf14aca177569c
wxtong1234 <wxtong1234@gmail.com>
2019-10-16 10:54:46 -0500

    Updated layout and celestial page
    Update the layout and celestial event page.

--- app/src/main/java/com/cs495/phototk/CelestialActivity.java
+++ app/src/main/java/com/cs495/phototk/CelestialActivity.java
@@ -1,14 +1,28 @@
 package com.cs495.phototk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CelestialActivity extends AppCompatActivity {
-
+    private Button btnSolar_Eclipse;
+    private static final String TAG = "CelestialActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_celestial);
+        Button btnSolar_Eclipse = findViewById(R.id.btnSolar_Eclipse);
+        btnSolar_Eclipse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CelestialActivity.this,SolarEclipse.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cs495/phototk/SolarEclipse.java
@@ -0,0 +1,13 @@
+package com.cs495.phototk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SolarEclipse extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.solar_eclipse);
+    }
+}
