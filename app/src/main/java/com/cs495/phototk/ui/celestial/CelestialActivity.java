84ecb1253e84280ee83c6e6cf2823e25250d20c5
minruihuang <minrui113@gmail.com>
2019-10-30 17:01:35 -0500

    finish the first navigation bar. work on the second one. may go with top navigation bar

--- app/src/main/java/com/cs495/phototk/ui/celestial/CelestialActivity.java
+++ app/src/main/java/com/cs495/phototk/ui/celestial/CelestialActivity.java
@@ -3,12 +3,19 @@ package com.cs495.phototk.ui.celestial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cs495.phototk.MainActivity;
 import com.cs495.phototk.R;
+import com.cs495.phototk.ui.exif.EXIFActivity;
+import com.cs495.phototk.ui.map.MapsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CelestialActivity extends AppCompatActivity {
     private Button btnSolar_Eclipse;
@@ -21,6 +28,39 @@ public class CelestialActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_celestial);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(3);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent0 = new Intent(CelestialActivity.this, MainActivity.class);
+                        startActivity(intent0);
+                        break;
+
+                    case R.id.ic_exif:
+                        Intent intent1 = new Intent(CelestialActivity.this, EXIFActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_celestial:
+
+                        break;
+
+                    case R.id.ic_map:
+                        Intent intent4 = new Intent(CelestialActivity.this, MapsActivity.class);
+                        startActivity(intent4);
+                        break;
+
+                }
+                return false;
+            }
+        });
+
         Button btnSolar_Eclipse = findViewById(R.id.btnSolar_Eclipse);
         btnSolar_Eclipse.setOnClickListener(new View.OnClickListener() {
             @Override
