58874c867a69f3b9328588995a3c7909886576f2
Brett Heithold <35582670+baheithold@users.noreply.github.com>
2019-12-03 15:40:53 -0600

    Added bottom navigation select listener

--- app/src/main/java/com/cs495/phototk/ui/user/UserActivity.java
+++ app/src/main/java/com/cs495/phototk/ui/user/UserActivity.java
@@ -1,16 +1,23 @@
 package com.cs495.phototk.ui.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cs495.phototk.MainActivity;
 import com.cs495.phototk.R;
+import com.cs495.phototk.ui.celestial.CelestialActivity;
+import com.cs495.phototk.ui.exif.EXIFActivity;
+import com.cs495.phototk.ui.map.MapsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -31,6 +38,45 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        // initialize bottom navigation bar
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        int size = bottomNavigationView.getMenu().size();
+        for (int i = 0; i < size; i++) {
+            bottomNavigationView.getMenu().getItem(i).setCheckable(false);
+        }
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent0 = new Intent(UserActivity.this, MainActivity.class);
+                        startActivity(intent0);
+                        overridePendingTransition(R.anim.slide_up, R.anim.no_anim);
+                        break;
+
+                    case R.id.ic_exif:
+                        Intent intent1 = new Intent(UserActivity.this, EXIFActivity.class);
+                        startActivity(intent1);
+                        overridePendingTransition(R.anim.slide_up, R.anim.no_anim);
+                        break;
+
+                    case R.id.ic_celestial:
+                        Intent intent3 = new Intent(UserActivity.this, CelestialActivity.class);
+                        startActivity(intent3);
+                        overridePendingTransition(R.anim.slide_up, R.anim.no_anim);
+                        break;
+
+                    case R.id.ic_map:
+                        Intent intent4 = new Intent(UserActivity.this, MapsActivity.class);
+                        startActivity(intent4);
+                        overridePendingTransition(R.anim.slide_up, R.anim.no_anim);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        // Initialize the rest of the activity
         init();
     }
 
