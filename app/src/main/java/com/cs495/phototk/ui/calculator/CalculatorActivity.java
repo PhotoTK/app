85059d9e8c8ad056bbd6e5dcfa7329e5edad0b42
brian31321 <55430872+brian31321@users.noreply.github.com>
2019-12-04 04:47:16 -0600

    implement calculator calculating exposure value feature

--- app/src/main/java/com/cs495/phototk/ui/calculator/CalculatorActivity.java
+++ app/src/main/java/com/cs495/phototk/ui/calculator/CalculatorActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,10 +18,14 @@ import com.cs495.phototk.MainActivity;
 import com.cs495.phototk.R;
 import com.cs495.phototk.ui.celestial.CelestialActivity;
 import com.cs495.phototk.ui.exif.EXIFActivity;
+import com.cs495.phototk.ui.management.GearEdit;
+import com.cs495.phototk.ui.management.ManagementActivity;
 import com.cs495.phototk.ui.map.MapsActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 /*READ ME: The following code in onCreate is for the navigation bar. Try not to modify it. In addition, change the activity_calculator_center.xml instead of changing activity_calculator.xml
  */
 
@@ -27,6 +35,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class CalculatorActivity extends AppCompatActivity {
     private static final String TAG = "CalculatorActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
+
+
+    TextView Exposure;
+    EditText Aperture;
+    EditText Speed;
+    EditText Iso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +85,42 @@ public class CalculatorActivity extends AppCompatActivity {
                         break;
                 }
                 return false;
+            }
+        });
+        button_Calculate();
+    }
+
+
+    public void button_Calculate() {
+        Button addGearButton = (Button) findViewById(R.id.button_time);
+        addGearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Exposure = (TextView) findViewById(R.id.EV);
+                Aperture = (EditText) findViewById(R.id.aperture);
+                if (Aperture.getText().toString().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please enter aperture", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Speed = (EditText) findViewById(R.id.speed);
+                if (Speed.getText().toString().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please enter speed", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Iso = (EditText) findViewById(R.id.iso);
+                if (Iso.getText().toString().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please enter iso", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float N = Float.valueOf(Aperture.getText().toString());
+                float t = Float.valueOf(Speed.getText().toString());
+                float ISO =Float.valueOf(Iso.getText().toString());
+                float EV = (float) ((Math.log(N * N) / Math.log(2)) + (Math.log(1 / t) / Math.log(2)) -
+                        (Math.log(ISO / 100) / Math.log(2)));
+                String tmp = String.format("EV = %.2f", EV);
+                Exposure.setText(tmp);
             }
         });
     }
