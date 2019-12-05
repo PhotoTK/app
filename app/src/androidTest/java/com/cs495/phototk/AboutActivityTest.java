eaad9bfe60f145153873bd9a1b3e59b86f796270
brian31321 <55430872+brian31321@users.noreply.github.com>
2019-12-05 09:21:19 -0600

    about activity test case

---
+++ app/src/androidTest/java/com/cs495/phototk/AboutActivityTest.java
@@ -0,0 +1,68 @@
+package com.cs495.phototk;
+
+import android.app.Activity;
+
+import com.cs495.phototk.ui.AboutActivity;
+import com.cs495.phototk.ui.management.GearEdit;
+import android.app.Instrumentation;
+import android.view.View;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.os.SystemClock;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.anything;
+import static org.junit.Assert.assertNotNull;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class AboutActivityTest {
+
+    @Rule
+    public ActivityTestRule<AboutActivity> mActivityRule = new ActivityTestRule(AboutActivity.class);
+    private AboutActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityRule.getActivity();
+    }
+
+    @Test
+    public void testVideoButton()
+    {
+        View view = mActivity.findViewById(R.id.btnvideo);
+        assertNotNull(view);
+        onView(withId(R.id.btnvideo)).perform(click());
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
