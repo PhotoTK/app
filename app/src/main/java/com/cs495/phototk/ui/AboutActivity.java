f8496f876bd8b83bfe8b3801308bd8d0b08f7476
brian31321 <55430872+brian31321@users.noreply.github.com>
2019-12-05 08:54:46 -0600

    Video linked to about us

--- app/src/main/java/com/cs495/phototk/ui/AboutActivity.java
+++ app/src/main/java/com/cs495/phototk/ui/AboutActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.cs495.phototk.MainActivity;
 import com.cs495.phototk.R;
@@ -57,6 +61,16 @@ public class AboutActivity extends AppCompatActivity {
                         break;
                 }
                 return false;
+            }
+        });
+       Button btnvideo = (Button) findViewById(R.id.btnvideo);
+        btnvideo.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/g2eFdxq-ntE")));
+                Log.i("Video", "Video Playing....");
+
             }
         });
     }
